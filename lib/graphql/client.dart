11b3a026f06d7f61bcd3d2f88db5ac7cb2f10c18
himanushi <himanushi@users.noreply.github.com>
2021-08-05 05:19:32 +0900

    graphql url 修正

--- lib/graphql/client.dart
+++ lib/graphql/client.dart
@@ -1,6 +1,6 @@
 import 'package:gql_http_link/gql_http_link.dart';
 import 'package:ferry/ferry.dart';
 
-final link = HttpLink("http://localhost:3000");
+final link = HttpLink("http://localhost:3000/graphql");
 
 final client = Client(link: link);
