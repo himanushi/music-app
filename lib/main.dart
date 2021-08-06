6cc932d403c437ec3932e615d4476c28a385a349
himanushi <himanushi@users.noreply.github.com>
2021-08-07 05:34:01 +0900

    graphql 実行して値を取得

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:music_app/graphql/queries/album.req.gql.dart';
+import 'package:music_app/graphql/schema.schema.gql.dart';
+import 'graphql/client.dart' show client;
 
 void main() => runApp(MyApp());
 
@@ -38,8 +41,34 @@ class _HomeState extends State<Home> {
         title: Text(AppLocalizations.of(context)!.title),
       ),
       body: Center(
-        child: Text(AppLocalizations.of(context)!.title),
+        child: AlbumName(),
       ),
     );
+  }
+}
+
+class AlbumName extends StatefulWidget {
+  const AlbumName({Key? key}) : super(key: key);
+
+  @override
+  _AlbumNameState createState() => _AlbumNameState();
+}
+
+class _AlbumNameState extends State<AlbumName> {
+  String albumName = "";
+
+  final request = GAlbumReq(
+      (bulder) => bulder..vars.id = new GTTID("abm171f7ec1b0aa4").toBuilder());
+
+  @override
+  Widget build(BuildContext context) {
+    client.request(request).listen((result) => {
+          setState(() => {
+                if (result.data?.album?.name != null)
+                  {albumName = result.data!.album!.name}
+              })
+        });
+
+    return Text(albumName);
   }
 }
