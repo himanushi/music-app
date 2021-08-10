e08fb5ba5bc3e8b8c641ef1a383fe56658aee6fe
himanushi <himanushi@users.noreply.github.com>
2021-08-11 07:39:20 +0900

    blowser クッキー対応

--- lib/graphql/client.dart
+++ lib/graphql/client.dart
@@ -1,6 +1,20 @@
 import 'package:gql_http_link/gql_http_link.dart';
 import 'package:ferry/ferry.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/browser_client.dart';
 
-final link = HttpLink("http://localhost:3000/graphql");
+// ref: https://github.com/gql-dart/ferry/issues/145
+dynamic getClient() {
+  final httpClient = http.Client();
 
+  // blowser のみクッキーを保持する
+  if (httpClient is BrowserClient) {
+    httpClient.withCredentials = true;
+  }
+
+  return httpClient as dynamic;
+}
+
+final httpClient = getClient(); // use this for REST requests
+final link = HttpLink("http://localhost:3000/graphql", httpClient: httpClient);
 final client = Client(link: link);
