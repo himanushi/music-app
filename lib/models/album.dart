5491e6ff2a8f195a206daa8ac779bc6a5103105f
himanushi <himanushi@users.noreply.github.com>
2021-08-11 06:26:44 +0900

    取得データ表示

--- lib/models/album.dart
+++ lib/models/album.dart
@@ -1,1 +1,29 @@
-class Album {}
+import 'package:music_app/graphql/client.dart';
+import 'package:music_app/graphql/queries/album.data.gql.dart';
+import 'package:music_app/graphql/queries/album.req.gql.dart';
+import 'package:music_app/graphql/schema.schema.gql.dart';
+
+class Album {
+  String? id;
+
+  where({required String id}) {
+    this.id = id;
+    return this;
+  }
+
+  execute(void Function(GAlbumData_album) callback) {
+    final id = this.id;
+    if (id == null) {
+      throw 'where を指定してください';
+    } else {
+      // "abm171f7ec1b0aa4"
+      final reviewsReq = GAlbumReq((b) => b..vars.id = GTTID(id).toBuilder());
+      client.request(reviewsReq).listen((result) {
+        if (result.data?.album != null) {
+          final album = result.data!.album!;
+          callback(album);
+        }
+      });
+    }
+  }
+}
--- lib/pages/albums/albums.dart
+++ lib/pages/albums/albums.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:music_app/graphql/queries/album.data.gql.dart';
+import 'package:music_app/models/album.dart';
 import 'package:music_app/widgets/app/footer.dart';
 
 class AlbumsPage extends StatefulWidget {
@@ -9,8 +11,20 @@ class AlbumsPage extends StatefulWidget {
 }
 
 class _AlbumsPageState extends State<AlbumsPage> {
+  String albumName = "";
+
+  void fetch() {
+    Album().where(id: "abm171f7ec1b0aa4").execute((GAlbumData_album album) {
+      setState(() {
+        print(album.name);
+        albumName = album.name;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("test"), bottomNavigationBar: Footer());
+    fetch();
+    return Scaffold(body: Text(albumName), bottomNavigationBar: Footer());
   }
 }
